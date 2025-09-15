5e4ebd2b6ec75bfe46a98f839d1901a5bafc14d3
carloscabral656 <edu.carlos.cec@gmail.com>
2025-09-15 12:30:25 -0300

    creating new ed

--- main.py
+++ main.py
@@ -1,29 +1,46 @@
+from typing import List, Optional, Any, Dict
 from fastapi import FastAPI
 from fastapi import HTTPException
 from fastapi import status
 from fastapi.responses import JSONResponse
 from fastapi import Path
+from fastapi import Query
 from fastapi import Response
+from fastapi import Header
 from models import Course
+from time import sleep
+from fastapi import Depends
+from models import courses_list
 
-app = FastAPI()
+from routes import course_router
+from routes import user_router
 
-courses = {
-    1: {
-        "title": "Programação para Leigos",
-        "clases": 112,
-        "hours": 58
-    },
-    2: {
-        "title": "Algoritmos e Lógica de Programação",
-        "clases": 87,
-        "hours": 67
-    }
-}
+app = FastAPI(
+    title="API de Teste", 
+    version="0.0.1", 
+    description="Uma API para estudo do FastAPI"
+)
 
-@app.get("/courses")
-async def get_courses():
-    return courses
+app.include_router(course_router.router, tags=["courses"])
+app.include_router(user_router.router, tags=["users"])
+
+def fake_db():
+    try:
+        print("Abrindo conexão com o banco de dados")
+        sleep(1)
+    finally:
+        print("Fechando conexão com banco de dados")
+        sleep(1)
+
+
+@app.get("/courses", 
+        description="Retorna a lista de todos os cursos", 
+        summary="Retorna todos os cursos", 
+        response_model=List[Course],
+        response_description="Cursos encontrados com sucesso"
+)
+async def get_courses(db: Any = Depends(fake_db)):
+    return courses_list
 
 """
     End-point que possui parâmetro.
@@ -35,24 +52,22 @@ async def get_courses():
 async def get_course_by_id(course_id: int = Path(title="ID do curso", description="Deve ser entre 1 e 2", gt=0, lt=3)):
     ## Todo o input do Python é string, então precsiamos fazer o type cast.
     try:
-        course = courses[course_id]
+        course = courses_list[course_id]
         return course
     except KeyError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Couse not found.")
 
-
-@app.post("/courses", status_code=status.HTTP_201_CREATED)
+@app.post("/courses", status_code=status.HTTP_201_CREATED, response_model=Course)
 async def create_course(course: Course):
-    next_id = len(courses) + 1
+    next_id = len(courses_list) + 1
     course.id = next_id
-    courses[next_id] = course
+    courses_list.append(course )
     return course
-
 
 @app.put("/courses/{course_id}")
 async def update_course(course_id: int , course: Course):
-    if course_id in courses:
-        courses[course_id] = course
+    if course_id in courses_list:
+        courses_list[course_id] = course
         course.id = course_id
         return course
     else:
@@ -60,14 +75,18 @@ async def update_course(course_id: int , course: Course):
     
 @app.delete("/courses/{course_id}")
 async def delete_course(course_id: int): 
-    if course_id in courses:
-        del courses[course_id]
+    if course_id in courses_list:
+        del courses_list[course_id]
         # return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
         return Response(status_code=status.HTTP_204_NO_CONTENT)
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Couse not found.")
 
+@app.get("/calculate")
+async def calculate(a: int = Query(default=None, gt=5), b: int = Query(default=None, gt=10), x_geek: str = Header(default=None)):
+    print(f'X-GEEK: {x_geek}')
+    return a + b
+
 if __name__ == '__main__':
     import uvicorn
-
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 class Course(BaseModel):
     id: Optional[int] = None
@@ -7,4 +7,25 @@ class Course(BaseModel):
     clases: int
     hours: int
 
+    @validator('title')
+    def validate_title(cls, value):
+        words = value.split(" ")
+        if(len(words) < 3):
+            raise ValueError("The title must have at least 3 words.")
+        return value
 
+
+courses = {
+    1: {
+        "title": "Programação para Leigos",
+        "clases": 112,
+        "hours": 58
+    },
+    2: {
+        "title": "Algoritmos e Lógica de Programação",
+        "clases": 87,
+        "hours": 67
+    }
+}
+
+courses_list = [Course(id=key, **value) for key, value in courses.items()]
